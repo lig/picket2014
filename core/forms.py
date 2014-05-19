aac888b61db21f11f3dd837569ccabc155d1da7c
Serge Matveenko <s@matveenko.ru>
2014-05-19 19:59:56 +0400

    Prepare new issue form.

---
+++ core/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+
+class IssueForm(forms.Form):
+    pass
--- core/views.py
+++ core/views.py
@@ -1,8 +1,25 @@
 from actionviews import TemplateView
+
 from .documents import Issue
+from .forms import IssueForm
 
 
 class IssueView(TemplateView):
 
     def do_list(self: ''):
         return {'issues': Issue.objects.all()}
+
+    def do_create(self):
+        request = self.request
+
+        if request.method == 'POST':
+            form = IssueForm(request.POST)
+
+            if form.is_valid():
+                issue = Issue(**form.cleaned_data)
+                issue.save()
+
+        else:
+            form = IssueForm()
+
+        return {'form': form}
